36e77fc376fa3cc87206ec53b58036831fdff297
mmarcus11 <mmarcus@brandeis.edu>
2018-04-06 12:17:00 -0400

    got smooth moving dude

--- game.js
+++ game.js
@@ -2,22 +2,62 @@ Physijs.scripts.worker = '/js/physijs_worker.js';
 Physijs.scripts.ammo = '/js/ammo.js';
 var scene = new Physijs.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 16;
+camera.position.z = 30;
 
-// camera.position.y=4;
 camera.lookAt(0,0,0);
 var renderer = new THREE.WebGLRenderer();
 var floor;
 var wall;
-var cube;
+var dude;
 
 var light1 = new THREE.AmbientLight( 0xffffff,0.25);
 var light2 = new THREE.SpotLight( 0xffffff );
 var light3 = new THREE.SpotLight( 0xffff00 );
 
+var controls = {
+    left: false,
+    right: false,
+    forward: false,
+    backward: false
+};
+
+window.addEventListener("keydown", function(event){
+    switch(event.key)
+    {
+        case("s"):{
+          controls.forward = true;
+        }; break;
+        case("w"):{
+          controls.backward = true;
+        }; break;
+        case("a"):{
+          controls.left = true;
+        }; break;
+        case("d"):{
+          controls.right = true;
+        }; break;
+    };
+});
+
+window.addEventListener("keyup", function(event){
+    switch(event.key)
+    {
+        case("s"):{
+          controls.forward = false;
+        }; break;
+        case("w"):{
+          controls.backward = false;
+        }; break;
+        case("a"):{
+          controls.left = false;
+        }; break;
+        case("d"):{
+          controls.right = false;
+        }; break;
+    };
+});
 
 function init(){
-
     renderer.setSize( window.innerWidth, window.innerHeight );
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = THREE.PCFSoftShadowMap;
@@ -37,12 +77,12 @@ function init(){
 
     floor     = new Physijs.BoxMesh( floorGeometry, pfloorMat, 0);
     wall1     = new THREE.Mesh( wallGeometry, wallMaterial );
-    cube      = new Physijs.BoxMesh( cubeGeometry, pcubeMat, 1 );
+    dude      = new Physijs.BoxMesh( cubeGeometry, pcubeMat, 1 );
 
 
     // cube.setDamping(0.1,0.1);
-    cube.castShadow = true;
-    cube.receiveShadow = false;
+    dude.castShadow = true;
+    dude.receiveShadow = false;
     floor.castShadow = false;
     floor.receiveShadow = true;
     wall1.castShadow = false;
@@ -66,25 +106,52 @@ function init(){
     floor.position.z = -1;
     wall1.position.y = 2;
     wall1.position.z = -10;
-    cube.position.z = 4;
+    dude.position.z = 4;
 
     scene.add( floor );
-    scene.add( cube );
+    scene.add( dude );
     scene.add( light1 );
     scene.add( light2 )
     // scene.add( light3 )
     scene.add( wall1 );
-
-
 }
 
+
+function updateDude(){
+  // print(dude.position);
+  if(controls.left){
+    //dude.setLinearVelocity(dude.getWorldDirection().multiplyScalar(5))
+    dude.translateX(-1);
+    dude.__dirtyPosition = true;
+  }
+  if(controls.right){
+    dude.translateX(1);
+    dude.__dirtyPosition = true;
+  }
+  if(controls.forward){
+    dude.translateZ(1);
+    dude.__dirtyPosition = true;
+  }
+  if(controls.backward){
+    dude.translateZ(-1);
+    dude.__dirtyPosition = true;
+  }
+
+  // if(!controls.left && !controls.right && !controls.forward && !controls.backward)
+  // {
+  //   dude.setLinearVelocity(new THREE.Vector3(0, 0, 0));
+  // }
+}
 var counter = 0;
 
 function animate() {
     requestAnimationFrame( animate );
+    updateDude();
     scene.simulate();
-
     renderer.render( scene, camera );
+
+    //dude.setLinearVelocity(dude.getWorldDirection().multiplyScalar(5))
+    dude.__dirtyPosition = false;
 }
 init();
 animate();
